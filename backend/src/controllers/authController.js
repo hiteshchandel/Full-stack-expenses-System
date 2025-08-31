867123ff03c69b03a2d8f0e18e5d699cfe967ac8
Hitesh Chandel <hiteshchandel858@gmail.com>
2025-08-31 16:41:16 +0530

    create login api and use in frontend

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -17,6 +17,25 @@ exports.signup = async (req, res) => {
             user: newUser
         });
     } catch (error) {
+        console.log(error);
         return res.status(500).json({ message: "Internal server error", error: error.message });
     }
-}
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        return res.status(200).json({ message: "Login successful", user });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
